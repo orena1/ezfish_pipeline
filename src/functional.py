fix rotation and mask matching

src/functional.py
@@ -28,9 +28,7 @@ def extract_suite2p_registered_planes(manifest: dict , session: dict):
     save_path_registered = Path(manifest['base_path']) / manifest['mouse_name'] / 'OUTPUT' / '2P' / 'registered'
     save_path.mkdir(exist_ok=True, parents=True)
     functional_plane = session['functional_plane'][0]
-    print('1232 loading ops')
     ops = np.load(suite2p_path / 'plane0/ops.npy',allow_pickle=True).item()
-    print('12d32 finish - loading ops')
     planes = ops['nplanes']
 
     for plane in track(range(planes), description='Extracting suite2p registered planes'):
@@ -75,12 +73,12 @@ def extract_suite2p_registered_planes(manifest: dict , session: dict):
     data = tif_imread(save_filename_C01)
     assert data.ndim == 2, 'suite2p binary files should be 2D, not 3D!, can fix but not implemented yet'
     for k in rotation_config:
-        if k == 'rotation':
+        if k == 'rotation' and rotation_config[k]:
             data = rotate(data, rotation_config['rotation'])
-        if k == 'fliplr':
-            data = data[::-1,:]
-        if k == 'flipud':
-            data =data[:,::-1]
+        if k == 'fliplr' and rotation_config[k]:
+            data = data[:,::-1]
+        if k == 'flipud' and rotation_config[k]:
+            data =data[::-1,:]
     
 
     tif_imwrite(save_filename_rotated, 

src/segmentation.py
@@ -149,7 +149,7 @@ def extract_probs_intensities(manifest):
         csv_output_path = output_folder / f"{round_folder_name}_probs_intensities.csv"
         pkl_output_path = output_folder / f"{round_folder_name}_probs_intensities.pkl"
         if pkl_output_path.exists():
-            print(f"Intensities already extracted for {round_folder_name} - skipping")
+            print(f"HCR - Intensities already extracted for {round_folder_name} - skipping")
             continue
 
         
@@ -231,7 +231,7 @@ def extract_electrophysiology_intensities(manifest: dict , session: dict):
     for plane in [functional_plane]:
         pkl_save_path = save_path / f'lowres_meanImg_C0_plane{plane}.pkl'
         if pkl_save_path.exists():
-            print(f"Intensities already extracted for plane {plane} - skipping")
+            print(f"2p activity already extracted for plane {plane} - skipping")
             continue
 
         # Set up binary file
@@ -345,19 +345,22 @@ def align_masks(manifest: dict, session: dict):
 
     cellpose_path = Path(manifest['base_path']) / manifest['mouse_name'] / 'OUTPUT' / '2P' / 'cellpose'
     stats = np.load(cellpose_path / f'lowres_meanImg_C0_plane{plane}_seg.npy', allow_pickle=True).item()
-    masks_2p = stats['masks']  # Get (x, y) indices per mask
+    
+    from IPython import embed; embed()
+    stats = np.load(cellpose_path / f'lowres_meanImg_C0_plane{plane}_seg.npy', allow_pickle=True).item()
 
+    masks_2p = np.array(stats['masks'])  # Get (x, y) indices per mask
     for k in rotation_config:
-        if k == 'rotation':
-            masks_2p = np.stack([rotate(masks_2p[0], rotation_config['rotation']),rotate(masks_2p[1], rotation_config['rotation'])])
-        if k == 'fliplr':
-            masks_2p = masks_2p[:,::-1,:]
-        if k == 'flipud':
-            masks_2p =masks_2p[:,:,::-1]
+        if k == 'rotation' and rotation_config[k]:
+            masks_2p = rotate(masks_2p, rotation_config['rotation'])
+        if k == 'fliplr' and rotation_config[k]:
+            masks_2p = masks_2p[:,::-1]
+        if k == 'flipud' and rotation_config[k]:
+            masks_2p = masks_2p[::-1,:]
 
 
     masks_2p_rotated = masks_2p
-    masks_2p_rotated_path = cellpose_path / f'lowres_meanImg_C0_plane{plane}_seg_rotated.tiff'
+    masks_2p_rotated_path = cellpose_path / f'lowres_meanImg_C0_plane{plane}_seg_rotated_11.tiff'
     tif_imsave(masks_2p_rotated_path,  masks_2p_rotated)
 
     masks_2p_rotated_to_HCR1 = cellpose_path / f'lowres_meanImg_C0_plane{plane}_seg_rotated_to_HCR.tiff'

src/tiling.py
@@ -147,11 +147,11 @@ def stitch_tiles_and_rotate(manifest: dict, session: dict):
     rotation_config = hjson.load(open(rotation_file,'r'))
     data = tif_imread(stitched_file_C01)
     for k in rotation_config:
-        if k == 'rotation':
+        if k == 'rotation' and rotation_config[k]:
             data = np.stack([rotate(data[0], rotation_config['rotation']),rotate(data[1], rotation_config['rotation'])])
-        if k == 'fliplr':
-            data = data[:,::-1,:]
-        if k == 'flipud':
-            data =data[:,:,::-1]
+        if k == 'fliplr' and rotation_config[k]:
+            data = data[:, :, ::-1]
+        if k == 'flipud' and rotation_config[k]:
+            data =data[:, ::-1, :]
     tif_imwrite(stitched_file_C01.parent / f'{stitched_file_C01.stem}_rotated.tiff', data.astype(np.float32), imagej=True, metadata={'axes': 'CYX'})
     tif_imwrite(save_path_registered_rotated, data.astype(np.float32), imagej=True, metadata={'axes': 'CYX'})